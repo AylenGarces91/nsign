3544d2b4130a3124bffe7049f3ae2e9c2c38bd29
acastillo456 <acastillo@develoop.net>
2021-05-21 10:12:49 -0400

    No agrupar compra y proyectos

---
+++ custom_modules/stock_rule_custom/__manifest__.py
@@ -0,0 +1,17 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "No Agrupar Pedidos de Compra y Proyectos Custom",
+    'version': '0.1',
+    'author': "Develoop Software",
+    'category': 'Uncategorized',
+    'summary': 'No agrupar pedidos de compra + separar proyectos',
+    'website': "https://www.develoop.net/",
+    'description': """
+        No agrupar pedidos de compra + separar proyectos
+        """,
+    'depends': ['purchase_stock','sale_timesheet'],
+    'data': [
+    ],
+    'demo': [],
+    'installable': True,
+}
