be6f92ff8d22bac6ade67816c4fdff79c6910e3d
acastillo456 <acastillo@develoop.net>
2021-06-11 11:24:33 -0400

    Eliminar asientos contables

--- custom_modules/delete_invoice_custom/models/account_invoice.py
+++ custom_modules/delete_invoice_custom/models/account_invoice.py
@@ -3,7 +3,7 @@ from odoo import models, fields, api
 
 
 class AccountInvoice(models.Model):
-    _inherit = 'account.invoice'
+    _inherit = 'account.move'
     
     @api.multi
     def unlink(self):
