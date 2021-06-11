11329564acf63c286de3e0eada337ef82b049e1c
acastillo456 <acastillo@develoop.net>
2021-06-11 11:26:39 -0400

    Eliminar asientos

--- custom_modules/delete_invoice_custom/models/account_invoice.py
+++ custom_modules/delete_invoice_custom/models/account_invoice.py
@@ -5,7 +5,6 @@ from odoo import models, fields, api
 class AccountInvoice(models.Model):
     _inherit = 'account.move'
     
-    @api.multi
     def unlink(self):
         for record in self:
             if move.state == 'draft':
