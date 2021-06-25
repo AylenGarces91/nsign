93e8658036ef2869401e3dbe78ec309477732140
acastillo456 <acastillo@develoop.net>
2021-06-25 08:27:01 -0400

    Hubspot

--- custom_modules/odoo_hubspot_integration/models/project_project.py
+++ custom_modules/odoo_hubspot_integration/models/project_project.py
@@ -8,7 +8,7 @@ class ProjectProject_HubSpot(models.Model):
     
     _inherit = "project.project"
 
-    hubspot_so_id = fields.Char("Id HubSpot", compute="compute_get_hubspot_id")
+    hubspot_so_id = fields.Char("Id HubSpot", compute="compute_get_hubspot_id", store=True)
     
     def compute_get_hubspot_id(self):
         for record in self:
