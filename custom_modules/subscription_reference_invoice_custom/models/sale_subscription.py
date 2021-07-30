2ab9624e0985d4911033da07ac12475b1b9a48c6
acardozo <acardozo@develoop.net>
2021-07-30 12:16:07 -0400

    Update reference subscription
    - Se cambio el valor obtenido de la referencia de la orden de venta

--- custom_modules/subscription_reference_invoice_custom/models/sale_subscription.py
+++ custom_modules/subscription_reference_invoice_custom/models/sale_subscription.py
@@ -8,5 +8,6 @@ class SaleSubscription_reference_custom(models.Model):
 
     def _prepare_invoice_line(self, line, fiscal_position, date_start=False, date_stop=False):
         response = super(SaleSubscription_reference_custom, self)._prepare_invoice_line(line, fiscal_position, date_start=False, date_stop=False)
-        response['ref'] = self.code if self.code else False;
+        sale_order = self.env['sale.order'].search([('order_line.subscription_id', 'in', self.ids)], order="id desc", limit=1)
+        response['ref'] = sale_order.client_order_ref if sale_order else False;
         return response
