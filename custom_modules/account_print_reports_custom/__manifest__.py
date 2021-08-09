ee9fd75a3e74fddb69f44ba8ed89c743ac97e597
acardozo <acardozo@develoop.net>
2021-08-09 17:33:31 -0400

    Custom Reports Print
    - modulo con las modificaciones a los impresos de compra, venta y factura

---
+++ custom_modules/account_print_reports_custom/__manifest__.py
@@ -0,0 +1,25 @@
+{
+    'name': 'Account Print Reports custom',
+    'version': '0.0',
+    'summary': """Modificacion del dominio.""",
+    'description': """
+        Modulo personalizado de modificacion de reportes impresos de facturas, compras y ventas.
+    """,
+    'depends': ['base','account', 'sale', 'purchase'],
+    'data': [
+        'views/report_invoice_custom.xml',
+        'views/invoice_custom.xml',
+        'views/report_sale_order_custom.xml',
+        'views/sale_order_custom.xml',
+        'views/report_purchase_order_custom.xml',
+        'views/purchase_order_custom.xml',
+    ],
+    'author': 'Develoop Software',
+    'images': ['static/description/icon.png'],
+    'maintainer': 'Develoop Software',
+    'website': 'https://www.develoop.net',
+    'demo': [],
+    'installable': True,
+    'application': False,
+    'auto_install': False
+}
