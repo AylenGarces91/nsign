e12d2fa04f8ab7bfaf2f72e374a51a79e52fc92d
Rene Huanco <rhuanco@develoop.net>
2021-09-08 19:07:00 -0400

    Fix issue: - Fix file manifest version and format code - add space in views.xml

--- custom_modules/omc_show_origin/__manifest__.py
+++ custom_modules/omc_show_origin/__manifest__.py
@@ -1,30 +1,18 @@
 # -*- coding: utf-8 -*-
 {
     'name': "Show document origin",
-
     'summary': """
         Show document origin, without method created document 
         """,
-
     'description': """
         Show description in any case remove property validation
     """,
-
-    'author': "Develoop",
+    'author': "Develoop Software",
     'website': "https://develoop.net",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/13.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
-    'category': 'Uncategorized',
-    'version': '0.1',
-
-    # any module necessary for this one to work correctly
+    'category': 'Develoop',
+    'version': '13.0.1.0.0',
     'depends': ['base', 'purchase'],
-
-    # always loaded
     'data': [
-        # 'security/ir.model.access.csv',
         'views/views.xml',
     ],
 }
