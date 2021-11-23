b1413181cecf361d1cc71a3a686039a898b1516d
Juan Flores <jflores@develoop.net>
2021-11-23 09:04:07 -0400

    Fecha efectiva de albaran sincronizado con fecha movimientos

---
+++ custom_modules/stock_tweaks/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Agregados Inventarios',
+    'version': '1.1',
+    'summary': 'Modificaciones comportamiento de inventarios',
+    'description': """
+    Fecha efectiva de albarán editable y sincronizada con movimientos generados. 
+    """,
+    'author': 'DEVELOOP',
+    'category': 'stock',
+    'website': 'https://www.develoop.net',
+    'depends': ['base', 'stock'],
+    'data': [
+        'views/stock_picking_views.xml'
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
