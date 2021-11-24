1b62170345f92af56afbd79699404a1fdc32ff58
Juan Flores <jflores@develoop.net>
2021-11-24 11:17:15 -0400

    Modulo con reporte en blanco

---
+++ custom_modules/dev_subscription_print/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+{
+    'name': 'Imprimir Subscripciones',
+    'version': '1.1',
+    'summary': 'Report para subscripciones',
+    'description': """
+    
+    """,
+    'author': 'DEVELOOP',
+    'category': 'sale',
+    'website': 'https://www.develoop.net',
+    'depends': ['base', 'sale_subscription'],
+    'data': [
+        'reports/sale_subscription_report.xml',
+    ],
+    'demo': [
+    ],
+    'qweb': [
+    ],
+    'installable': True,
+    'application': False,
+    'auto_install': False,
+}
---
+++ custom_modules/dev_subscription_print/models/sale_subscription.py
@@ -0,0 +1,5 @@
+from odoo import models, fields, api
+
+
+class SaleSubscription(models.Model):
+    _inherit = 'sale.subscription'
